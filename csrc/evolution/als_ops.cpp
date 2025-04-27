42ca3da5f8517378985c063265cec9460b7b20fe
Addison Richards <adsrichards@outlook.com>
2025-04-27 14:45:20 -0400

    add cutensor r2 build

--- csrc/evolution/als_ops.cpp
+++ csrc/evolution/als_ops.cpp
@@ -96,16 +96,16 @@ torch::Tensor cutensor_build_s1(torch::Tensor n12g, torch::Tensor a2r) {
     static int64_t bD = a2r.size(1);
     static int64_t pD = a2r.size(2);
 
-    static const std::vector<int64_t> extentA_s1 = {nD, nD, pD, pD};
-    static const std::vector<int64_t> extentB_s1 = {nD, bD, pD};
-    static const std::vector<int64_t> extentC_s1 = {nD, bD, pD};
+    static const std::vector<int64_t> extentA = {nD, nD, pD, pD};
+    static const std::vector<int64_t> extentB = {nD, bD, pD};
+    static const std::vector<int64_t> extentC = {nD, bD, pD};
 
-    static const std::vector<int32_t> modeA_s1 = {'Y','X','p','Q'};
-    static const std::vector<int32_t> modeB_s1 = {'X','U','Q'};
-    static const std::vector<int32_t> modeC_s1 = {'Y','U','p'};
+    static const std::vector<int32_t> modeA = {'Y','X','p','Q'};
+    static const std::vector<int32_t> modeB = {'X','U','Q'};
+    static const std::vector<int32_t> modeC = {'Y','U','p'};
 
-    static CutensorContraction s1(modeA_s1, modeB_s1, modeC_s1, 
-                                  extentA_s1, extentB_s1, extentC_s1);
+    static CutensorContraction s1(modeA, modeB, modeC, 
+                                  extentA, extentB, extentC);
     return s1.build(n12g, a2r);
 }
 
@@ -114,51 +114,80 @@ torch::Tensor cutensor_build_s2(torch::Tensor n12g, torch::Tensor a1r) {
     static int64_t bD = a1r.size(1);
     static int64_t pD = a1r.size(2);
 
-    static const std::vector<int64_t> extentA_s2 = {nD, nD, pD, pD};
-    static const std::vector<int64_t> extentB_s2 = {nD, bD, pD};
-    static const std::vector<int64_t> extentC_s2 = {nD, bD, pD};
+    static const std::vector<int64_t> extentA = {nD, nD, pD, pD};
+    static const std::vector<int64_t> extentB = {nD, bD, pD};
+    static const std::vector<int64_t> extentC = {nD, bD, pD};
 
-    static const std::vector<int32_t> modeA_s2 = {'Y','X','P','q'};
-    static const std::vector<int32_t> modeB_s2 = {'Y','V','P'};
-    static const std::vector<int32_t> modeC_s2 = {'X','V','q'};
+    static const std::vector<int32_t> modeA = {'Y','X','P','q'};
+    static const std::vector<int32_t> modeB = {'Y','V','P'};
+    static const std::vector<int32_t> modeC = {'X','V','q'};
 
-    static CutensorContraction s2(modeA_s2, modeB_s2, modeC_s2, 
-                                  extentA_s2, extentB_s2, extentC_s2);
+    static CutensorContraction s2(modeA, modeB, modeC,
+                                  extentA, extentB, extentC);
     return s2.build(n12g, a1r);
 }
-
 
 torch::Tensor cutensor_build_r1(torch::Tensor n12, torch::Tensor a2r) {
     static int64_t nD = a2r.size(0);
     static int64_t bD = a2r.size(1);
     static int64_t pD = a2r.size(2);
 
-    static const std::vector<int64_t> extentA_r1_intm = {nD, nD, nD, nD};
-    static const std::vector<int64_t> extentB_r1_intm = {nD, bD, pD};
-    static const std::vector<int64_t> extentC_r1_intm = {nD, nD, nD, bD, pD};
+    static const std::vector<int64_t> extentA_intm = {nD, nD, nD, nD};
+    static const std::vector<int64_t> extentB_intm = {nD, bD, pD};
+    static const std::vector<int64_t> extentC_intm = {nD, nD, nD, bD, pD};
 
-    static const std::vector<int32_t> modeA_r1_intm = {'y','x','Y','X'};
-    static const std::vector<int32_t> modeB_r1_intm = {'x','u','q'};
-    static const std::vector<int32_t> modeC_r1_intm = {'y','Y','X','u','q'};
+    static const std::vector<int32_t> modeA_intm = {'y','x','Y','X'};
+    static const std::vector<int32_t> modeB_intm = {'x','u','q'};
+    static const std::vector<int32_t> modeC_intm = {'y','Y','X','u','q'};
 
-    static CutensorContraction r1_intm(modeA_r1_intm, modeB_r1_intm, modeC_r1_intm, 
-                                       extentA_r1_intm, extentB_r1_intm, extentC_r1_intm);
+    static CutensorContraction r1_intm(modeA_intm, modeB_intm, modeC_intm, 
+                                       extentA_intm, extentB_intm, extentC_intm);
 
-    static const std::vector<int64_t> extentA_r1 = {nD, nD, nD, bD, pD};
-    static const std::vector<int64_t> extentB_r1 = {nD, bD, pD};
-    static const std::vector<int64_t> extentC_r1 = {nD, bD, nD, bD};
+    static const std::vector<int64_t> extentA = {nD, nD, nD, bD, pD};
+    static const std::vector<int64_t> extentB = {nD, bD, pD};
+    static const std::vector<int64_t> extentC = {nD, bD, nD, bD};
 
-    static const std::vector<int32_t> modeA_r1 = {'y','Y','X','u','q'};
-    static const std::vector<int32_t> modeB_r1 = {'X','U','Q'};
-    static const std::vector<int32_t> modeC_r1 = {'Y','U','y','u'};
+    static const std::vector<int32_t> modeA = {'y','Y','X','u','q'};
+    static const std::vector<int32_t> modeB = {'X','U','Q'};
+    static const std::vector<int32_t> modeC = {'Y','U','y','u'};
 
-    static CutensorContraction r1(modeA_r1, modeB_r1, modeC_r1, 
-                                  extentA_r1, extentB_r1, extentC_r1);
+    static CutensorContraction r1(modeA, modeB, modeC, 
+                                  extentA, extentB, extentC);
     return r1.build(r1_intm.build(n12, a2r), a2r);
+}
+
+torch::Tensor cutensor_build_r2(torch::Tensor n12, torch::Tensor a1r) {
+    static int64_t nD = a1r.size(0);
+    static int64_t bD = a1r.size(1);
+    static int64_t pD = a1r.size(2);
+
+    static const std::vector<int64_t> extentA_intm = {nD, nD, nD, nD};
+    static const std::vector<int64_t> extentB_intm = {nD, bD, pD};
+    static const std::vector<int64_t> extentC_intm = {nD, nD, nD, bD, pD};
+
+    static const std::vector<int32_t> modeA_intm = {'y','x','Y','X'};
+    static const std::vector<int32_t> modeB_intm = {'y','v','p'};
+    static const std::vector<int32_t> modeC_intm = {'x','Y','X','v','p'};
+
+    static CutensorContraction r2_intm(modeA_intm, modeB_intm, modeC_intm, 
+                                       extentA_intm, extentB_intm, extentC_intm);
+
+    static const std::vector<int64_t> extentA = {nD, nD, nD, bD, pD};
+    static const std::vector<int64_t> extentB = {nD, bD, pD};
+    static const std::vector<int64_t> extentC = {nD, bD, nD, bD};
+
+    static const std::vector<int32_t> modeA = {'x','Y','X','v','P'};
+    static const std::vector<int32_t> modeB = {'Y','V','P'};
+    static const std::vector<int32_t> modeC = {'X','V','x','v'};
+
+    static CutensorContraction r2(modeA, modeB, modeC, 
+                                  extentA, extentB, extentC);
+    return r2.build(r2_intm.build(n12, a1r), a1r);
 }
 
 TORCH_LIBRARY(cutensor_ext, m) {
     m.def("build_s1", &cutensor_build_s1);
     m.def("build_s2", &cutensor_build_s2);
     m.def("build_r1", &cutensor_build_r1);
+    m.def("build_r2", &cutensor_build_r2);
 }
--- csrc/evolution/benchmark.py
+++ csrc/evolution/benchmark.py
@@ -14,7 +14,7 @@ def make_inputs(nD, bD, pD):
 
     return n12, n12g, a2r
 
-def measure_time(build_fn, A, B, nwarmup=50, niter=1000):
+def measure_time(build_fn, A, B, nwarmup=50, niter=10):
     for _ in range(nwarmup):
         build_fn(A, B)
     torch.cuda.synchronize()
@@ -27,36 +27,29 @@ def measure_time(build_fn, A, B, nwarmup=50, niter=1000):
     torch.cuda.synchronize()
     return start_evt.elapsed_time(end_evt) / niter, C
 
-def benchmark_build_s1():
-    nD, bD, pD = 16, 6, 2
+def benchmark_build_s1(nD, bD, pD):
     _, n12g, a2r = make_inputs(nD, bD, pD)
 
     cutensor_time, C = measure_time(torch.ops.cutensor_ext.build_s1, n12g, a2r)
-    torch_time, C_true = measure_time(lambda A, B: torch.einsum('YXpQ,XUQ->YUp', A, B), n12g, a2r)
+    torch_time, C_true = measure_time(s1_reference, n12g, a2r)
     C_true = C_true.permute(2,1,0).reshape(nD,bD,pD)
 
     rel_error = (C_true - C).abs().max() / C_true.abs().max()
     print(f"[build_s1] error = {rel_error.item():.3e}")
     print(f"[build_s1] cuTENSOR: {cutensor_time:.4f} ms, torch.einsum: {torch_time:.4f} ms")
 
-def benchmark_build_s2():
-    nD, bD, pD = 64, 8, 2
+def benchmark_build_s2(nD, bD, pD):
     _, n12g, a2r = make_inputs(nD, bD, pD)
 
     cutensor_time, C = measure_time(torch.ops.cutensor_ext.build_s2, n12g, a2r)
-    torch_time, C_true = measure_time(lambda A, B: torch.einsum('YXPq,YVP->XVq', A, B), n12g, a2r)
+    torch_time, C_true = measure_time(s2_reference, n12g, a2r)
     C_true = C_true.permute(2,1,0).reshape(nD,bD,pD)
 
     rel_error = (C_true - C).abs().max() / C_true.abs().max()
     print(f"[build_s2] error = {rel_error.item():.3e}")
     print(f"[build_s2] cuTENSOR: {cutensor_time:.4f} ms, torch.einsum: {torch_time:.4f} ms")
 
-def r1_reference(A, B):
-    R = torch.einsum('yxYX,xuq->yYXuq', A, B)
-    return torch.einsum('yYXuq,XUQ->YUyu', R, B)
-
-def benchmark_build_r1():
-    nD, bD, pD = 16, 6, 2
+def benchmark_build_r1(nD, bD, pD):
     n12, _, a2r = make_inputs(nD, bD, pD)
 
     cutensor_time, C = measure_time(torch.ops.cutensor_ext.build_r1, n12, a2r)
@@ -67,10 +60,37 @@ def benchmark_build_r1():
     print(f"[build_r1] error = {rel_error.item():.3e}")
     print(f"[build_r1] cuTENSOR: {cutensor_time:.4f} ms, torch.einsum: {torch_time:.4f} ms")
 
+def benchmark_build_r2(nD, bD, pD):
+    n12, _, a1r = make_inputs(nD, bD, pD)
+
+    cutensor_time, C = measure_time(torch.ops.cutensor_ext.build_r2, n12, a1r)
+    torch_time, C_true = measure_time(r2_reference, n12, a1r)
+    C_true = C_true.permute(3,2,1,0).reshape(nD,bD,nD,bD)
+
+    rel_error = (C_true - C).abs().max() / C_true.abs().max()
+    print(f"[build_r2] error = {rel_error.item():.3e}")
+    print(f"[build_r2] cuTENSOR: {cutensor_time:.4f} ms, torch.einsum: {torch_time:.4f} ms")
+
+def s1_reference(A, B):
+    return torch.einsum('YXpQ,XUQ->YUp', A, B)
+
+def s2_reference(A, B):
+    return torch.einsum('YXPq,YVP->XVq', A, B)
+
+def r1_reference(A, B):
+    R = torch.einsum('yxYX,xuq->yYXuq', A, B)
+    return torch.einsum('yYXuq,XUQ->YUyu', R, B)
+
+def r2_reference(A, B):
+    R = torch.einsum('yxYX,yvp->xYXvp', A, B)
+    return torch.einsum('xYXvP,YVP->XVxv', R, B)
+
 def main():
-    benchmark_build_s1()
-    benchmark_build_s2()
-    benchmark_build_r1()
+    nD, bD, pD = 16, 8, 2
+    benchmark_build_s1(nD, bD, pD)
+    benchmark_build_s2(nD, bD, pD)
+    benchmark_build_r1(nD, bD, pD)
+    benchmark_build_r2(nD, bD, pD)
 
 if __name__ == "__main__":
     main()
